fe03430b9535646cd3732680383de0fe9ef5e688
raifborges-dev <raiborges96@gmail.com>
2025-03-17 01:09:25 -0300

    Update server.js

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -17,12 +17,4 @@ app.use('/users', login);
 app.use('/rooms', auth, rooms)
 app.use('/schedules', auth, schedules)
 
-
-//raiborges
-//nbbRY8G5jRF7ZPsa
-
-//mongodb+srv://<db_username>:<db_password>@testes.bmyim.mongodb.net/?retryWrites=true&w=majority&appName=Testes
-
-
-
 app.listen(3000, () => console.log("Servidor iniciado!"));
